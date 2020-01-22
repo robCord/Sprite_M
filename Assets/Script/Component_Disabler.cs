ab77566ebbe2b92e150a6bf5dbd6abb1bbd44c9b
Roberto <rob_cord@aol.com>
2020-01-22 12:26:14 -0700

    Added assets from classwork

---
+++ Assets/Script/Component_Disabler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Component_Disabler : MonoBehaviour
+{
+    public Sprite_Mover SpriteMover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteMover = gameObject.GetComponent<Sprite_Mover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when p is pressed down it disables sprite mover until p is pressed again
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SpriteMover.enabled = (!SpriteMover.enabled);
+        }
+    }
+}
---
+++ Assets/Script/Sprite_Mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            //only works when shift is held down
+        }
+        else
+        {
+            //only when shift is not held down
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                //tf.position = tf.position + Vector3.up * time.deltaTime * speed;
+            }
+        }
+        //if player is holding down shift or isn't holding down shift
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //tf.position = Vector3(0, 0, 0);
+        }
+    }
+
+}
